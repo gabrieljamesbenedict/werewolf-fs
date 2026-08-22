919292645d64a4e9a7f1c4659d13dbbb614eec15
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-23 02:42:21 +0800

    style: add index.css and remove margin and padding from main html element

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import './styles/index.css'
 import Router from './router'
 
 createRoot(document.getElementById('root')).render(
