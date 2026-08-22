a8c14a1a7823e227848a953d8f09cec8b158ebe5
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-23 02:53:05 +0800

    feat: add labels and text inputs for username and roomId in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,20 @@ import React from 'react'
 const Home = () => {
 
     const werewolfTitle = "Welcome to Werewolf FS"
-    const werewolfDescription = "Werewolf is a popular social deduction party game where Werewolves kill one-by-one at night while Villagers try to deduce who is the enemy."
+    const werewolfDescription = "Werewolf is a popular social deduction party game where Werewolves kill Villagers one-by-one at night while Villagers try to deduce who are the Werewolves."
 
     return (
-        <div>
+        <div style={{padding: 16}}>
             <h1>{werewolfTitle}</h1>
             <p>{werewolfDescription}</p>
+            <br />
+            <label htmlFor="">Username: </label>
+            <input type="text" />
+            <br />
+            <label htmlFor="">Room ID: </label>
+            <input type="text" />
+            <br />
+            <button>Enter Game</button>
         </div>
     )
 }
