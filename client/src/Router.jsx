d87f74507e18b646aea81eaf17dea1561cbb6a2f
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-23 02:37:17 +0800

    feat: add SPA page routing with Router.jsx

---
+++ client/src/Router.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Router = () => {
+
+    const [page, setPage] = useState('home')
+
+  return (
+    <div>Router</div>
+  )
+}
+
+export default Router
