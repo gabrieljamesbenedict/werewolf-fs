45e3c197aaca83cccf63d3506c3b99621ce77927
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-24 04:36:46 +0800

    feat: replace existing logic with react-router-dom logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
 const App = () => {
 
-    const [page, setPage] = useState('home')
     const [username, setUsername] = useState('')
     const [roomId, setRoomId] = useState('')
 
     return (
-        <Home username={username} roomId={roomId} setUsername={setUsername} setRoomId={setRoomId} />
+        <Routes>
+            <Route path="/" element={<Home username={username} roomId={roomId} setUsername={setUsername} setRoomId={setRoomId} />} />
+        </Routes>
     )
 }
 
