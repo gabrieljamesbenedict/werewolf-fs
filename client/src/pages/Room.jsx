ec22e024ef575bb6ab51bbf3e0cdf52ad70cc3da
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-24 13:33:23 +0800

    feat: add roomId and display it in Room.jsx

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { io } from 'socket.io-client'
 
 const Room = ({ username }) => {
 
     const { roomId } = useParams()
 
-  return (
-    <div>Your room id is {roomId}</div>
-  )
+    const [playerList, setPlayerList ] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(()=> {
+      const socket = io('http://localhost:3001')
+
+      return () => {
+        socket.disconnect()
+      }
+    },[])
+
+    const leaveRoom = () => {
+      navigate("/")
+    }
+
+    return (
+      <div>
+        <button onClick={() => leaveRoom()}>Back to Main</button>
+        <p>Your room id is {roomId}</p>
+        <p>Player List</p>
+        {
+          playerList.map(
+            (item, index) => (
+              <p key={index}>{item}</p>
+            )
+          )
+        }
+      </div>
+    )
 }
 
 export default Room
