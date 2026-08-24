2e4f9e8011fd908d8d450aa13f94303698a3fcb6
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-24 19:17:47 +0800

    chore: add logging in RoomService

--- server/web/service/RoomService.js
+++ server/web/service/RoomService.js
@@ -1,21 +1,26 @@
 import * as RoomRepository from '../repository/RoomRepository.js'
 
 export const getRoom = (roomId) => {
+    console.log(`Finding room with id=${roomId}`);
     return RoomRepository.findRoomById(roomId)
 }
 
 export const getAllRooms = () => {
+    console.log(`Finding all rooms`);
     return RoomRepository.findAllRooms()
 }
 
 export const createRoom = () => {
+    console.log(`Creating a new room`);
     return RoomRepository.createRoom()
 }
 
 export const updateRoom = (roomId, room) => {
+    console.log(`Updating room with id=${roomId}`);
     return RoomRepository.saveRoom(roomId, room)
 }
 
 export const deleteRoom = (roomId) => {
+    console.log(`Deleting room with id=${roomId}`);
     return RoomRepository.deleteRoom(roomId)
 }
