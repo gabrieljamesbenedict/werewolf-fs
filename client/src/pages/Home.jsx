0ff25ae836f366dc450a9c8241d9eabed68f6e06
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-24 13:31:46 +0800

    fix: move useNavigate hook at the top-level of the component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import "../styles/Home.css"
 
 
@@ -8,23 +8,25 @@ const Home = ({username, roomId, setUsername, setRoomId}) => {
     const werewolfTitle = "Welcome to Werewolf FS"
     const werewolfDescription = "Werewolf is a popular social deduction party game where Werewolves kill Villagers one-by-one at night while Villagers try to deduce who are the Werewolves."
 
+    const navigate = useNavigate()
+
     const enterRoom = (roomId) => {
         navigate(`/room/${roomId}`)
     }
 
     return (
-        <div class="container">
-            <h1 class="container__title">{werewolfTitle}</h1>
-            <p class="container__description">{werewolfDescription}</p>
+        <div>
+            <h1>{werewolfTitle}</h1>
+            <p>{werewolfDescription}</p>
 
-            <div class="container__form">
-                <label class="container__label">Username: </label>
-                <input class="container__input" type="text" autoComplete="off" value={username} onChange={e => setUsername(e.target.value)} />
+            <div>
+                <label>Username: </label>
+                <input type="text" autoComplete="off" value={username} onChange={e => setUsername(e.target.value)} />
                 <br />
-                <label class="container__label">Room ID: </label>
-                <input class="container__input" type="text" autoComplete="off" value={roomId} onChange={e => setRoomId(e.target.value)} />
+                <label>Room ID: </label>
+                <input type="text" autoComplete="off" value={roomId} onChange={e => setRoomId(e.target.value)} />
                 <br />
-                <button class="container__but" onClick={() => enterRoom(roomId)}>Enter Game</button>
+                <button onClick={() => enterRoom(roomId)}>Enter Game</button>
             </div>
         </div>
     )
