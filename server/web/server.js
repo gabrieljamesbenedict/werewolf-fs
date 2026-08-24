056a76e59d48dd8a21a6c2745c487b67cf27b368
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-24 16:19:04 +0800

    fix: allow CORS origin from anywhere

--- server/web/server.js
+++ server/web/server.js
@@ -1,7 +1,12 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import RoomController from './controller/RoomController.js'
 
 const fastify = Fastify()
+
+await fastify.register(cors, { 
+    origin: '*' 
+})
 
 fastify.get('/', async () => ({ hello: 'world' }))
 fastify.register(RoomController)
