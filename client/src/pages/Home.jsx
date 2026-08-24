9f884e87662825383ef286191e955979dd716181
Gabriel James Benedict <benok.loslos@gmail.com>
2026-08-24 13:26:49 +0800

    feat: update username and roomId states on input onChange

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 import "../styles/Home.css"
+
 
 const Home = ({username, roomId, setUsername, setRoomId}) => {
 
     const werewolfTitle = "Welcome to Werewolf FS"
     const werewolfDescription = "Werewolf is a popular social deduction party game where Werewolves kill Villagers one-by-one at night while Villagers try to deduce who are the Werewolves."
+
+    const enterRoom = (roomId) => {
+        navigate(`/room/${roomId}`)
+    }
 
     return (
         <div class="container">
@@ -12,12 +19,12 @@ const Home = ({username, roomId, setUsername, setRoomId}) => {
 
             <div class="container__form">
                 <label class="container__label">Username: </label>
-                <input class="container__input" type="text" />
+                <input class="container__input" type="text" autoComplete="off" value={username} onChange={e => setUsername(e.target.value)} />
                 <br />
                 <label class="container__label">Room ID: </label>
-                <input class="container__input" type="text" />
+                <input class="container__input" type="text" autoComplete="off" value={roomId} onChange={e => setRoomId(e.target.value)} />
                 <br />
-                <button class="container__but">Enter Game</button>
+                <button class="container__but" onClick={() => enterRoom(roomId)}>Enter Game</button>
             </div>
         </div>
     )
